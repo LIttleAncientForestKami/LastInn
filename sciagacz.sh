eb0c3bdf18545472276dc1a2972e6c9c37ba6edf
LittleAncientForestKami <Tomasz.Borek@gmail.com>
2012-09-12 09:04:55 +0200

    Dublowanie wyeliminowane, -k też.

--- sciagacz.sh
+++ sciagacz.sh
@@ -3,8 +3,8 @@
 BASE_URL="http://lastinn.info/sesje-rpg-fantasy/3725-sesja-dwanascie-masek"
 EXT=".html"
 
-wget -q $BASE_URL$EXT
+# wget -k $BASE_URL$EXT
 
-for nr in $(seq 1 $1); do wget -q $BASE_URL"-"$nr$EXT; done
+for nr in $(seq 1 $1); do wget $BASE_URL"-"$nr$EXT; done
 
 # parsowanie cholerstwa by relatywne linki zrobić na absolutne.
